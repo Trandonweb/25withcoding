d50375727c8d1110686ae43b83fd4784bff89063
Trandon <trandonwebtoon@naver.com>
2026-08-07 10:19:12 +0900

    Update main.py

--- AI/backend/main.py
+++ AI/backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from ai import ask_ai
+
 
 app = FastAPI()
+
+
+class ChatRequest(BaseModel):
+    message: str
+
 
 @app.get("/")
 def home():
@@ -8,4 +17,14 @@ def home():
         "status": "online",
         "name": "Coby AI",
         "version": "0.1"
+    }
+
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+
+    answer = ask_ai(request.message)
+
+    return {
+        "answer": answer
     }
